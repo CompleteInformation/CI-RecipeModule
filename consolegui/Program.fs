128b193f5154164a2e63700687be2bdc37c3e9a3
Nico Linder <nicolinder@icloud.com>
2017-10-31 18:57:35 +0100

    add some more commands

--- consolegui/Program.fs
+++ consolegui/Program.fs
@@ -31,31 +31,55 @@ let splitCommandAndArguments input =
     | command::arguments -> (command, arguments)
 
 let showHelp() =
-    printfn "\nCompleteInformation - Recipes"
+    printfn "\nCompleteInformation - Recipes\n"
     printfn "Commands:"
-    printfn "help - Show this help page"
-    printfn "quit - Exit program\n"
+    printfn "create                       - Creates a new recipe and opens it for further editing"
+    printfn "help                         - Show this help page"
+    printfn "ingredients add <ingredient> - Adds given ingredient"
+    printfn "name set <name>              - Set a name for the loaded recipe"
+    printfn "quit                         - Exit program\n"
+    printfn "show                         - Shows the actually loaded recipe"
+    printfn "unload                       - Unloads loaded recipe"
 
-let handleInput recipeList input =
-    match (splitCommandAndArguments input) with
-    | ("help", []) ->
+let handleInput state input =
+    let (active, recipeList) = state
+    match (splitCommandAndArguments input, active) with
+    | (("create", []), _) ->
+        let recipe = createEmptyRecipe()
+        printfn "New recipe created and loaded"
+        (Some recipe, recipeList)
+    | (("name", ["set";name]), Some recipe) ->
+        let recipe = setName recipe name
+        printfn "New name set"
+        (Some recipe, recipeList)
+    | (("ingredients", ["add";ingredient]), Some recipe) ->
+        let recipe = addIngredient recipe ingredient
+        (Some recipe, recipeList)
+    | (("show", []), Some recipe) ->
+        getName recipe |> printfn "\n%s"
+        printfn "Ingredients:"
+        getIngredients recipe |> List.iter (printfn "%s")
+        printfn "Text:"
+        getText recipe |> printfn "%s\n"
+        state
+    | (("unload", []), Some recipe) ->
+        printfn "Recipe unloaded"
+        (None, recipe::recipeList)
+    | (("help", []), _) ->
         showHelp()
-        recipeList
-    | (command, args) ->
-        printfn "Invalid input"
-        printfn "Command: %s" command
-        printfn "Arguments:"
-        args |> List.iter (printfn "%s")
-        recipeList
+        state
+    | _ ->
+        printfn "Invalid input, use help to get a overview over valid commands"
+        state
 
-let rec mainLoop recipeList =
+let rec mainLoop state =
     let input = Console.ReadLine()
     match input with
     | "quit" -> 0
     | input ->
-        handleInput recipeList input
+        handleInput state input
         |> mainLoop
 
 [<EntryPoint>]
 let main argv =
-    mainLoop []
+    mainLoop (None, [])
--- recipes/Types.fs
+++ recipes/Types.fs
@@ -2,14 +2,21 @@ namespace CompleteInformation.Recipes.Types
 
 module Recipe =
     type T = {
+        name: string;
         ingredients: string list;
-        recipetext: string option
+        recipetext: string option;
     }
 
     let createEmptyRecipe () = {
+        name = "";
         ingredients = [];
-        recipetext = None
+        recipetext = None;
     }
+
+    let setName (recipe:T) name =
+        { recipe with name = name }
+
+    let getName recipe = recipe.name
 
     let addIngredient recipe ingredient =
         { recipe with ingredients = ingredient::recipe.ingredients }
