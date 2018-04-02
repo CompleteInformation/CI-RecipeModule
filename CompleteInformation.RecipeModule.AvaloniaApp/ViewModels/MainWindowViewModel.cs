ebbb3e1871d125bcac604400f5529eec8835d58a
NicoVIII <nicolinder@icloud.com>
2018-04-02 19:19:04 +0200

    next steps

--- CompleteInformation.RecipeModule.AvaloniaApp/ViewModels/MainWindowViewModel.cs
+++ CompleteInformation.RecipeModule.AvaloniaApp/ViewModels/MainWindowViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using CompleteInformation.RecipeModule.AvaloniaApp.Views;
 using CompleteInformation.RecipeModule.Core;
 
 namespace CompleteInformation.RecipeModule.AvaloniaApp.ViewModels
