908f5e8e9f63311da50cd96a77a880a2a2f438c7
NicoVIII <nicolinder@icloud.com>
2018-04-06 09:31:13 +0200

    do not show details, if activerecipe is empty

--- CompleteInformation.RecipeModule.AvaloniaApp/Converter/ArrayToStringConverter.cs
+++ CompleteInformation.RecipeModule.AvaloniaApp/Converter/ArrayToStringConverter.cs
@@ -9,7 +9,7 @@ namespace CompleteInformation.RecipeModule.AvaloniaApp.Converter
     {
         public static readonly ArrayToStringConverter Instance = new ArrayToStringConverter();
 
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (targetType != typeof(string)) {
                 throw new InvalidOperationException("The target must be a String");
@@ -18,9 +18,9 @@ namespace CompleteInformation.RecipeModule.AvaloniaApp.Converter
             return String.Join("\r\n", (string[])value);
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            throw new InvalidOperationException("ArrayToStringConverter can only be used OneWay.");
         }
     }
 }
---
+++ CompleteInformation.RecipeModule.AvaloniaApp/Converter/IsNotNullConverter.cs
@@ -0,0 +1,22 @@
+using Avalonia;
+using Avalonia.Markup;
+using System;
+using System.Globalization;
+
+namespace CompleteInformation.RecipeModule.AvaloniaApp.Converter
+{
+    public class IsNotNullConverter : IValueConverter
+    {
+        public static readonly IsNotNullConverter Instance = new IsNotNullConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value != null);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new InvalidOperationException("IsNotNullConverter can only be used OneWay.");
+        }
+    }
+}
