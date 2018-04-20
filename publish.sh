46b1854f1a03c1ad2d68e5e46878cce4bf4ac99f
Nico Linder <nicolinder@icloud.com>
2018-04-20 21:55:26 +0200

    further improve publish script

--- publish.sh
+++ publish.sh
@@ -6,4 +6,7 @@ RELEASE_PATH="./CompleteInformation.RecipeModule.AvaloniaApp/bin/Release/netcore
 cp -r "CompleteInformation.RecipeModule.ConsoleApp/bin/Release/netcoreapp2.0/publish/." "$RELEASE_PATH"
 cp "README.md" "$RELEASE_PATH"
 cp "LICENSE.txt" "$RELEASE_PATH"
-zip -r ./release.zip $RELEASE_PATH
+PRIOR_PATH=$PWD
+cd $RELEASE_PATH
+zip -r "$PRIOR_PATH/release.zip" .
+cd $PRIOR_PATH
